cde103bcfad5fd1c5a53bf7f959cb529f6465328
Leon <hq_li1982@hotmail.com>
2020-12-31 21:44:06 +0800

    offline tested

--- public/db.js
+++ public/db.js
@@ -1,25 +1,29 @@
-let db;
 
+//create indexedDB version 1
 const request = indexedDB.open('bugdge', 1);
 
-request.onupgradeneeded = (event) => {
-    const db = event.target.result;
+//create collection 'pending' with auto increment id
+request.onupgradeneeded = () => {
+    const db = request.result;
     db.createObjectStore('pending', { autoIncrement: true });
 };
 
-request.onsuccess = (event) => {
-    db = event.target.result;
-
+//check online status, if online sync data with remote database
+request.onsuccess = () => {
     if (navigator.onLine) {
         checkDatabase()
     }
 };
 
+//error info
 request.onerror = (event) => {
     console.log('Woops' + event.target.errorCode);
 };
 
+//save data in indexedDB
 function saveRecord(record) {
+
+    const db = request.result;
 
     const trasaction = db.transaction(['pending'], 'readwrite');
 
@@ -28,33 +32,34 @@ function saveRecord(record) {
     store.add(record);
 };
 
+//sync with remote database then clear indexedDB
 function checkDatabase() {
-    // open a transaction on your pending db
-    const transaction = db.transaction(["pending"], "readwrite");
-    // access your pending object store
-    const store = transaction.objectStore("pending");
-    // get all records from store and set to a variable
+
+    const db = request.result;
+
+    const transaction = db.transaction(['pending'], 'readwrite');
+
+    const store = transaction.objectStore('pending');
+
     const getAll = store.getAll();
 
     getAll.onsuccess = function () {
         if (getAll.result.length > 0) {
-            fetch("/api/transaction/bulk", {
-                method: "POST",
+            fetch('/api/transaction/bulk', {
+                method: 'POST',
                 body: JSON.stringify(getAll.result),
                 headers: {
-                    Accept: "application/json, text/plain, */*",
-                    "Content-Type": "application/json"
+                    Accept: 'application/json, text/plain, */*',
+                    'Content-Type': 'application/json'
                 }
             })
                 .then(response => response.json())
                 .then(() => {
-                    // if successful, open a transaction on your pending db
-                    const transaction = db.transaction(["pending"], "readwrite");
 
-                    // access your pending object store
-                    const store = transaction.objectStore("pending");
+                    const transaction = db.transaction(['pending'], 'readwrite');
 
-                    // clear all items in your store
+                    const store = transaction.objectStore('pending');
+
                     store.clear();
                 });
         }
--- public/service-worker.js
+++ public/service-worker.js
@@ -48,14 +48,14 @@ self.addEventListener('activate', function (event) {
 //fetch
 
 self.addEventListener('fetch', function (event) {
-    const { url } = event.request;
-    if (url.includes('/') || url.includes('/api/transaction')) {
+
+    if (event.request.url.includes('/api/transaction')) {
         event.respondWith(
             caches.open(DATA_CACHE_NAME).then(cache => {
                 return fetch(event.request)
                     .then(response => {
                         if (response.status === 200) {
-                            cache.put(url, response.clone());
+                            cache.put(event.request.url, response.clone());
                         }
                         return response;
                     })
@@ -65,13 +65,14 @@ self.addEventListener('fetch', function (event) {
             })
                 .catch(err => console.log(err))
         );
-    } else {
-        event.respondWith(
-            caches.open(CACHE_NAME).then(cache => {
-                return cache.match(event.request).then(response => {
-                    return response || fetch(event.request);
-                });
-            })
-        );
+        return;
     }
+    event.respondWith(
+        caches.open(CACHE_NAME).then(cache => {
+            return cache.match(event.request).then(response => {
+                return response || fetch(event.request);
+            });
+        })
+    )
+
 });
